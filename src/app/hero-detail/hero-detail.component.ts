630455f1e79758af500ccb1a034601e100d8c9af
fabiostevenazzi <fabio.stevenazzi@altran.it>
2020-10-29 12:10:36 +0100

    cp

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -23,6 +23,16 @@ export class HeroDetailComponent implements OnInit {
     this.getHero();
   }
 
+
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
   getHero(): void {
     const id = +this.route.snapshot.paramMap.get('id');
     this.heroService.getHero(id)
