7b8204390d25e711c5a5cd36d7d1ce94fc11332d
fabiostevenazzi <fabio.stevenazzi@altran.it>
2020-10-29 11:17:46 +0100

    cp

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,21 +9,26 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private heroService: HeroService) {}
 
   selectedHero: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  heroes = HEROES;
+  heroes: Hero[];
 
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
 
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
   ngOnInit(): void {
+    this.getHeroes();
   }
 
 }
